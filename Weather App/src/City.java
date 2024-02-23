11d9e78a579df9ad16902662b253434811d5e8bb
BAOUCHbaSalma <155727770+BAOUCHbaSalma@users.noreply.github.com>
2024-02-23 16:09:59 +0100

    j'ai terminee la class City

--- Weather App/src/City.java
+++ Weather App/src/City.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 public class City {
 
 
+
+
     public int getCityId() {
         return cityId;
     }
@@ -50,11 +52,11 @@ public class City {
     private int currentWindSpeed;
     public City(){}
     public City(int cityId,String cityName,int currentTemperature,int currentHumidity,int currentWindSpeed){
-          this.cityId=cityId;
-          this.cityName=cityName;
-          this.currentTemperature=currentTemperature;
-          this.currentHumidity=currentHumidity;
-          this.currentWindSpeed=currentWindSpeed;
+        this.cityId=cityId;
+        this.cityName=cityName;
+        this.currentTemperature=currentTemperature;
+        this.currentHumidity=currentHumidity;
+        this.currentWindSpeed=currentWindSpeed;
     }
     private static final String URL = "jdbc:mysql://localhost/weather_app?serverTimeZone=UTC";
     private static final String USERNAME = "root";
@@ -69,10 +71,10 @@ public class City {
         String sql="INSERT INTO city (cityName,currentTemperature,currentHumidity,currentWindSpeed) values(?,?,?,?)";
         Connection connection = getConnection();
         PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setString(1,city.cityName);
-        statement.setInt(2,city.currentTemperature);
-        statement.setInt(3,city.currentHumidity);
-        statement.setInt(4,city.currentWindSpeed);
+        statement.setString(1,city.getCityName());
+        statement.setInt(2,city.getCurrentTemperature());
+        statement.setInt(3,city.getCurrentHumidity());
+        statement.setInt(4,city.getCurrentWindSpeed());
         statement.executeUpdate();
         connection.close();
         statement.close();
@@ -103,16 +105,66 @@ public class City {
 
 
 
-    public static void UpdateCity()throws SQLException{
+    public static void UpdateCity(City city)throws SQLException{
+        String sql="UPDATE  city SET cityName = ?, currentTemperature = ?, currentHumidity =? , currentWindSpeed=? WHERE cityId=?";
+        Connection connection=getConnection();
+        PreparedStatement statement=connection.prepareStatement(sql);
+        statement.setString(1,city.getCityName());
+        statement.setInt(2,city.getCurrentTemperature());
+        statement.setInt(3,city.getCurrentHumidity());
+        statement.setInt(4,city.getCurrentWindSpeed());
+        statement.setInt(5,city.getCityId());
+        statement.executeUpdate();
+        connection.close();
+        statement.close();
+        System.out.println("Ville mise a jour avec succes");
 
     }
 
-    public static void DeleteCity()throws SQLException{
+    public static void DeleteCity(City city)throws SQLException{
+        String sql="DELETE FROM city WHERE cityId=?";
+        Connection connection=getConnection();
+        PreparedStatement statement=connection.prepareStatement(sql);
+        statement.setInt(1,city.cityId);
+        statement.executeUpdate();
+        connection.close();
+        statement.close();
+        System.out.println("ville est suprimee avec succes");
+
+
 
     }
 
 
+
+
+    public static void SearchCity(String name)throws SQLException{
+        ArrayList<City> search = new ArrayList<>();
+        String sql="SELECT * FROM city WHERE cityName = ?";
+        Connection connection=getConnection();
+        PreparedStatement statement=connection.prepareStatement(sql);
+        statement.setString(1,name);
+        ResultSet resultSet= statement.executeQuery();
+        while (resultSet.next()){
+            int cityId= resultSet.getInt("cityId");
+            String cityName=resultSet.getString("cityName");
+            int currentTemperature=resultSet.getInt("currentTemperature");
+            int currentHumidity=resultSet.getInt("currentHumidity");
+            int currentWindSpeed=resultSet.getInt("currentWindSpeed");
+            search.add(new City(cityId,cityName,currentTemperature,currentHumidity,currentWindSpeed));
+            System.out.println("Id: "+cityId+"\n Name: "+cityName+"\n currentTemperature: "+currentTemperature+"\n currentHumidity: "+currentWindSpeed);
+        }
+
+        connection.close();
+        statement.close();
+
+
+
+
     }
+
+
+}
 
 
 
--- Weather App/src/Menu.java
+++ Weather App/src/Menu.java
@@ -1,10 +1,12 @@
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Menu {
     public void ListChoix() throws SQLException {
         int Choix;
         City city=new City();
+
 
         do{
             System.out.println("1--Ajouter une ville ");
@@ -15,44 +17,57 @@ public class Menu {
             System.out.println("6--Afficher un historique d'une ville ");
             System.out.println("7--Modifier un historique d'une ville ");
             System.out.println("8--Supprimer un historique d'une ville ");
-            System.out.println("9--Quitter");
-             Choix=new Scanner(System.in).nextInt();
-             switch (Choix){
-                 case 1:
-                     System.out.println("Entrez le nom de la ville :");
-                     city.setCityName(new Scanner(System.in).nextLine());
-                     System.out.println("Entrez la température actuelle");
-                     city.setCurrentTemperature(new Scanner(System.in).nextInt());
-                     System.out.println("Entrez le taux d'humidité actuelle");
-                     city.setCurrentHumidity(new Scanner(System.in).nextInt());
-                     System.out.println("Entrez la vitesse du vent actuelle");
-                     city.setCurrentWindSpeed(new Scanner(System.in).nextInt());
-                     City.AddCity(city);
+            System.out.println("9--Chercher une ville");
+            System.out.println("10--Chercher Historique d'une ville");
+            System.out.println("11--Quitter");
+            Choix=new Scanner(System.in).nextInt();
+            switch (Choix){
+                case 1:
+                    System.out.println("Entrez le nom de la ville :");
+                    city.setCityName(new Scanner(System.in).nextLine());
+                    System.out.println("Entrez la température actuelle");
+                    city.setCurrentTemperature(new Scanner(System.in).nextInt());
+                    System.out.println("Entrez le taux d'humidité actuelle");
+                    city.setCurrentHumidity(new Scanner(System.in).nextInt());
+                    System.out.println("Entrez la vitesse du vent actuelle");
+                    city.setCurrentWindSpeed(new Scanner(System.in).nextInt());
+                    City.AddCity(city);
 
-                     break;
-                 case 2:
-                     System.out.println("_________________Les villes_____________________ ");
-                     for(City ct : City.ShowCity()){
-                         System.out.println("                                                 ");
-                         System.out.println("id Ville : "+ct.getCityId());
-                         System.out.println("Nom ville : "+ct.getCityName());
-                         System.out.println("Température actuelle : "+ct.getCurrentTemperature());
-                         System.out.println("Taux d'humidité actuelle : "+ct.getCurrentHumidity());
-                         System.out.println("Vitesse du vent actuelle : "+ct.getCurrentWindSpeed());
-                         System.out.println("____________________________________________________");
+                    break;
+                case 2:
+                    System.out.println("_________________Les villes_____________________ ");
+                    for(City ct : City.ShowCity()){
+                        System.out.println("                                                 ");
+                        System.out.println("id Ville : "+ct.getCityId());
+                        System.out.println("Nom ville : "+ct.getCityName());
+                        System.out.println("Température actuelle : "+ct.getCurrentTemperature());
+                        System.out.println("Taux d'humidité actuelle : "+ct.getCurrentHumidity());
+                        System.out.println("Vitesse du vent actuelle : "+ct.getCurrentWindSpeed());
+                        System.out.println("____________________________________________________");
 
-                     }
+                    }
 
-                     break;
-                 case 3:
-                     City.UpdateCity();
-                     break;
-                 case 4:
-                     City.DeleteCity();
-                     break;
-                 case 5:
-                     CityHistory.AddHistory();
-                     break;
+                    break;
+                case 3:
+                    System.out.println("id");
+                    city.setCityId(new Scanner(System.in).nextInt());
+                    System.out.println("Entrez le nouveau nom de la ville :");
+                    city.setCityName(new Scanner(System.in).nextLine());
+                    System.out.println("Entrez la nouvelle température actuelle");
+                    city.setCurrentTemperature(new Scanner(System.in).nextInt());
+                    System.out.println("Entrez le nouveau taux d'humidité actuelle");
+                    city.setCurrentHumidity(new Scanner(System.in).nextInt());
+                    System.out.println("Entrez la nouvelle vitesse du vent actuelle");
+                    city.setCurrentWindSpeed(new Scanner(System.in).nextInt());
+                    City.UpdateCity(city);
+                    break;
+                case 4:
+                    System.out.println("Entrez id du ville que vous voullez supprimer");
+                    city.setCityId(new Scanner(System.in).nextInt());
+                    City.DeleteCity(city);
+                    break;
+                case 5:
+                    break;
                  case 6:
                      CityHistory.ShowHistory();
                      break;
